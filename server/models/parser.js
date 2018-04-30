f80ccf1c6706bf2796430ad55923f5dcb2ddb231
raphael <raph.koh@gmail.com>
2018-04-29 23:49:54 -0400

    Create parser for courses

---
+++ server/models/parser.js
@@ -0,0 +1,86 @@
+const fs = require('fs');
+
+// Finds first elem with substr and removes all preceding elems
+function read(textArr, substr, start = 0, offset = 0) {
+  const index = textArr.findIndex((str, idx) => idx >= start && str.includes(substr));
+  return textArr.slice(index + offset);
+}
+
+function parseComponent(arr) {
+  if (arr.length < 7) return null;
+  const classNum = arr[0];
+  if (!classNum.length) return null;
+  const section = arr[1];
+  const type = arr[2];
+  const dayArr = arr[3].split(' ');
+  const day = dayArr[0];
+  const startTime = dayArr[1];
+  const endTime = dayArr[3];
+  const location = arr[4].replace(/\s+/, ' ');
+  const instructor = arr[5].replace(/\n/g, ' ');
+  const dateArr = arr[6].split(' ');
+  const startDate = dateArr[0];
+  const endDate = dateArr[2];
+  return {
+    type,
+    info: {
+      classNum,
+      section,
+      day,
+      startTime,
+      endTime,
+      location,
+      instructor,
+      startDate,
+      endDate
+    }
+  };
+}
+
+function parseCourses(textArr) {
+  const courses = [];
+  let index = textArr.findIndex(str => str.includes('Status\t'));
+  while (index > -1) {
+    const courseArr = textArr[index - 1].split(' ');
+    textArr = textArr.slice(index);
+    const subject = courseArr[0];
+    const catalogNumber = courseArr[1];
+    index = textArr.findIndex(str => str.includes('Class Nbr\t'));
+    textArr = textArr.slice(index + 1);
+    const newIndex = textArr.findIndex(str => str.includes('Status\t'));
+    const classArr = textArr.slice(0, newIndex - 1);
+    index = newIndex;
+    const classes = {};
+    for (var i = 0; i < classArr.length; i += 7) {
+      const component = parseComponent(classArr.slice(i, i + 7));
+      if (component != null) classes[component.type] = component.info;
+    }
+    courses.push({
+      subject,
+      catalogNumber,
+      classes
+    });
+  }
+  return courses;
+}
+
+function parseText(callback) {
+  fs.readFile('./server/data/transcript.txt', 'utf8', (err, text) => {
+    if (err) throw err;
+
+    let textArr = text.split(/(?<!\,)\n/g);
+    textArr = read(textArr, '| University of Waterloo');
+
+    const term = textArr[0].slice(0, textArr[0].indexOf('|') - 1);
+    textArr = read(textArr, 'Status\t', 0, -1);
+
+    const courses = parseCourses(textArr);
+
+    callback({
+      term,
+      courses
+    });
+  });
+}
+
+module.exports = parseText;
