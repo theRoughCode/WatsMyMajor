37664d109e10283e24e8cd0ad0b18e2c1a5c7d94
raphael <raph.koh@gmail.com>
2018-07-09 22:20:34 -0700

    Fix schedule

--- react-ui/src/components/schedule/MySchedule.jsx
+++ react-ui/src/components/schedule/MySchedule.jsx
@@ -1,28 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
 import CalendarContainer from './CalendarContainer';
 import ParserInstructions from '../tools/ParserInstructions';
 
-const Schedule = () => {
-  const stepContents = [
-    {
-      button: 'Log in to Quest',
-      text: <p>Log in to your Quest account <a href="https://quest.pecs.uwaterloo.ca/psp/SS" target="_blank">here</a>.</p>
-    },
-    {
-      button: 'Enroll',
-      text: <p>Click on the <b>Enroll</b> tab.</p>
-    },
-    {
-      button: 'Select all and copy text',
-      text: <p>Select all, and copy EVERYTHING!!</p>
-    },
-  ];
+const stepContents = [
+  {
+    button: 'Log in to Quest',
+    text: <p>Log in to your Quest account <a href="https://quest.pecs.uwaterloo.ca/psp/SS" target="_blank">here</a>.</p>
+  },
+  {
+    button: 'Enroll',
+    text: <p>Click on the <b>Enroll</b> tab.</p>
+  },
+  {
+    button: 'Select all and copy text',
+    text: <p>Select all, and copy EVERYTHING!!</p>
+  },
+];
 
-  return (
-    <ParserInstructions stepContents={ stepContents } child={ <CalendarContainer /> } />
-  )
-};
+export default class Schedule extends Component {
+  state = {
+    text: '',
+    submitted: false,
+  };
 
+  onChange = (text) => this.setState({ text });
 
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.setState({ submitted: true });
+  }
 
-export default Schedule;
+  render() {
+    return (this.state.submitted)
+      ? <CalendarContainer text={this.state.text} />
+      : (
+          <div style={{ marginTop: 50 }}>
+            <ParserInstructions
+              onChange={ this.onChange }
+              stepContents={ stepContents }
+            />
+            <RaisedButton
+              label="Submit"
+              primary={ true }
+              onClick={ this.onSubmit }
+              disabled={ this.state.text.length === 0 }
+            />
+        </div>
+        )
+  }
+}
