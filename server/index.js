dd550dce0bfc8574abc81b419c05866c9c1cd2e4
raphael <raph.koh@gmail.com>
2018-07-18 23:47:36 -0700

    Integrate Raven for Sentry logging

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const Raven = require('raven');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
 
@@ -12,6 +13,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // Priority serve any static files.
 app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
+
+// Configure Raven for logging to Sentry
+Raven.config(process.env.SENTRY_DSN).install();
 
 // Connect all our routes to our application
 app.use('/', routes);
