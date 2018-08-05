ea4ed2b4cfea778ead258783a0e52c4a358f880b
raphael <raph.koh@gmail.com>
2018-08-05 14:49:48 -0700

    Implement default avatar

--- react-ui/src/components/sidebar/Avatar.jsx
+++ react-ui/src/components/sidebar/Avatar.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Avatar from 'material-ui/Avatar';
 
+const defaultAvatar = 'https://storage.googleapis.com/watsmymajor.appspot.com/profile-pictures/mr_goose.jpg?alt=media';
 
 const SideBarAvatar = ({ name, profileURL }) => (
 	<div className="avatar-wrapper">
 		<Avatar
 			className="avatar-icon"
-			src={ profileURL }
+			src={ (profileURL.length > 0) ? profileURL : defaultAvatar }
 			size={100}
 			style={{ objectFit: 'cover' }}
 		/>
