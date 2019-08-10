1c17beefc02c4c691ef597a90c829592fdb4ae1a
raphael <raph.koh@gmail.com>
2019-08-10 19:56:27 -0400

    Make prereq tree responsive

--- react-ui/src/components/courselist/tree/PrerequisitesTree.jsx
+++ react-ui/src/components/courselist/tree/PrerequisitesTree.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Tree from 'react-tree-graph';
-import Dimensions from 'react-dimensions'
+import Dimensions from 'react-dimensions';
 
 // We need a component because react-dimensions will complain if we use a
 // stateless presentational component.
@@ -24,7 +24,7 @@ class PrerequisitesTree extends Component {
         margins={{
           bottom: 50,
           left: 50,
-          right: 200,
+          right: 130,
           top: 50
         }}
         svgProps={{ className: 'tree' }}
